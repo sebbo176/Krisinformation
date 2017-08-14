adf6c215fa1549d9bd077e2df522833b6158deb5
Sebastian Bolling <sebastian.bolling@gmail.com>
2017-08-14 20:58:50 +0200

    Added areas to entry view

--- EntryView.js
+++ EntryView.js
@@ -26,14 +26,14 @@ var styles = StyleSheet.create({
     width: 400,
     height: 300
   },
-  price: {
-    fontSize: 25,
-    fontWeight: 'bold',
+  secondTitle: {
+    fontSize: 20,
     margin: 5,
-    color: '#48BBEC'
+    color: '#656565'
   },
   title: {
     fontSize: 20,
+    fontWeight: 'bold',
     margin: 5,
     color: '#656565'
   },
@@ -48,12 +48,16 @@ class EntryView extends Component {
 
   render() {
     var entry = this.props.entry;
+    var areas = Object.keys(entry.CapArea)
+    .map(key => (entry.CapArea[key].CapAreaDesc))
+    .join(', ');
 
     return (
       <View style={styles.container}>
         <View style={styles.heading}>
           <Text style={styles.title}>{entry.Title}</Text>
-          <Text style={styles.title}>{entry.Published.slice(0,10)}</Text>
+          <Text style={styles.secondTitle}>{entry.Published.slice(0,10)}</Text>
+          <Text style={styles.secondTitle}>{areas}</Text>
           <View style={styles.separator}/>
         </View>
         <Text style={styles.description}>{entry.Summary}</Text>
