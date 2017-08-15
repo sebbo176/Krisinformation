7369f0ec3c7c447f1cae975f980e0b228d804ef9
Sebastian Bolling <sebastian.bolling@gmail.com>
2017-08-15 21:03:10 +0200

    Added react maps to the entry view, BOOOOYAAAAA

--- EntryView.js
+++ EntryView.js
@@ -1,11 +1,13 @@
 'use strict';
 
 import React, { Component } from 'react'
+import MapView from 'react-native-maps'
 import {
   StyleSheet,
   Image,
   View,
-  Text
+  Text,
+  ScrollView,
 } from 'react-native';
 
 //maps api key
@@ -39,8 +41,30 @@ var styles = StyleSheet.create({
   },
   description: {
     fontSize: 18,
-    margin: 5,
+    margin: 0,
     color: '#656565'
+  },
+  MapContainer: {
+  position: 'absolute',
+  top: 355,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  height: 250,
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+},
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  scrollView: {
+  marginTop:0,
+  height:227,
+  alignContent:'flex-start'
   }
 });
 
@@ -52,6 +76,11 @@ class EntryView extends Component {
     .map(key => (entry.CapArea[key].CapAreaDesc))
     .join(', ');
 
+    var longitude = parseFloat(entry.CapArea[0].Coordinate.split(',')[0]);
+    var latitude = parseFloat(entry.CapArea[0].Coordinate.split(',')[1].split(' ')[0]);
+
+    console.log('longitude=' + longitude);
+    console.log('lat=' + latitude);
     return (
       <View style={styles.container}>
         <View style={styles.heading}>
@@ -60,8 +89,21 @@ class EntryView extends Component {
           <Text style={styles.secondTitle}>{areas}</Text>
           <View style={styles.separator}/>
         </View>
+        <ScrollView style={styles.scrollView}
+          contentInset={{top:0}}
+          automaticallyAdjustContentInsets={false}>
         <Text style={styles.description}>{entry.Summary}</Text>
-        <View style={styles.separator}/>
+        </ScrollView>
+        <View style={styles.MapContainer}>
+        <MapView style={styles.map}
+          initialRegion={{
+            latitude: latitude,
+            longitude: longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
       </View>
     );
   }
